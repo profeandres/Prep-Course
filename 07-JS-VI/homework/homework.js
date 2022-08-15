1b95acc5752858ec027b6dd5ae9808e17e81902a
profeandres <andrescantillo@hotmail.com>
2022-08-15 15:27:21 -0500

    last test made!

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,23 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  let name= nombre.split('')
+  name[0] = name[0].toUpperCase()
+  name=name.join("")
+  return name;
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb()
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1,n2)
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +28,16 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  let cont = 0;
+  numeros.map((el)=>{cont+=el})
+  return cb(cont)
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.map((el)=>{cb(el)})
 }
 
 function map(array, cb) {
@@ -35,12 +45,19 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+let newArray=[]
+  array.forEach(function(el){
+    newArray.push(cb(el))
+  });
+  return newArray
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  let newArray= array.filter((el) => el.charAt(0)==='a')
+  return newArray
 }
 
 // No modificar nada debajo de esta línea
